9d2a407a13b10213bb4f5a419b34d4189a38996b
Brisa Echeverría <111757558+ebrss@users.noreply.github.com>
2025-02-19 11:40:21 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="image/x-icon" href="logo.png" />
+    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Descubre Tu Spotify</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary"data-bs-theme="dark" >
+      <div class="container-fluid">
+        <a class="navbar-brand"> Descubre Tu Spotify </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <a class="nav-link active" aria-current="page" href="">Home</a>
+            <a class="nav-link" href="sections/recentSongs.php">Recent Songs</a>
+            <a class="nav-link" href="sections/topSongs.php">Top Songs</a>
+            <a class="nav-link" href="sections/topSitio.php">Top DescubreTuSpotify</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+
+    <div class="container">
+        <h1 id="welcome-title">Descubre Tu Spotify</h1>
+        <button id="login">Iniciar Sesión con Spotify</button>
+    </div>
+    
+    <div class="container">
+        <h1 id="welcome-title">Información importante</h1>
+        <h5 id="welcome-title">Si tiene cookies activadas no necesita iniciar sesión cada vez que accede al sitio</h5>
+    
+    <script src="script.js"></script>
+</body>
+</html>
---
+++ script.js
@@ -0,0 +1,51 @@
+document.addEventListener('DOMContentLoaded', () => {
+    //CLAVE API Y LINK REDIRECCIÓN LUEGO DEL LOGIN
+    const CLIENT_ID = '4c492a9b95534c7a9a022fdc563c4f71';
+    const REDIRECT_URI = 'https://descubretuspotify.000webhostapp.com/';
+
+
+    const loginButton = document.getElementById('login');
+    const scopes = 'user-read-recently-played user-read-private user-top-read';
+
+    //SE GUARDA EL TOKEN DE LA URL Y LUEGO SE ALMACENA LOCALMENTE
+    const getTokenFromURL = () => {
+        const hash = window.location.hash.substring(1);
+        const params = new URLSearchParams(hash);
+        return params.get('access_token');
+    };
+
+    const fetchUserInfo = (token) => {
+        fetch('https://api.spotify.com/v1/me', {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            const userName = data.display_name || data.id;
+            const country = data.country;
+            const userID = data.id;
+            const photoUrl = data.images && data.images.length > 0 ? data.images[0].url : null;
+            
+            localStorage.setItem('userName', userName);
+            localStorage.setItem('country', country);
+            localStorage.setItem('photo', photoUrl);
+            localStorage.setItem('userID', userID);
+            
+        })
+        .catch(error => console.error('Error:', error));
+    };
+
+    const accessToken = getTokenFromURL();
+
+    if (accessToken) {
+        localStorage.setItem('accessToken', accessToken);
+        fetchUserInfo(accessToken);
+        window.location = 'sections/recentSongs.php';
+    } 
+
+    loginButton.addEventListener('click', () => {
+        window.location = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=${scopes}&response_type=token&show_dialog=true`;
+    });
+});
+
